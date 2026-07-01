dba0cb887fdad29a1eea7ec1f0170f2f8ff990e0
najeeb75k-sys <najeeb75k@gmail.com>
2026-07-01 13:47:13 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE = 'fb-cargo-v5';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-72.png',
+  './icon-96.png',
+  './icon-128.png',
+  './icon-144.png',
+  './icon-152.png',
+  './icon-192.png',
+  './icon-384.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then((res) => {
+          return caches.open(CACHE).then((cache) => {
+            cache.put(e.request, res.clone());
+            return res;
+          });
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
